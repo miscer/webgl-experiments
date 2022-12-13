9f099dac6f5756e41ebf1df5383acb3ab72859ff
Michal Miškerník <michal@miskernik.sk>
2022-12-13 17:31:03 +0100

    Perspective rendering

--- eye/eye.ts
+++ eye/eye.ts
@@ -20,10 +20,16 @@ export function createEye(canvas: HTMLCanvasElement) {
   ctx.enableVertexAttribArray(positionAttrib);
 
   const projectionMatrix = mat4.create();
-  mat4.ortho(projectionMatrix, -3, 3, -3, 3, -3, 3);
-  
+  mat4.perspective(projectionMatrix, Math.PI / 4, canvas.width / canvas.height, 0.1, 10);
+
   const projectionMatrixUniform = ctx.getUniformLocation(program, "projection_matrix");
   ctx.uniformMatrix4fv(projectionMatrixUniform, false, projectionMatrix);
+
+  const viewMatrix = mat4.create();
+  mat4.translate(viewMatrix, viewMatrix, [0, 0, -6]);
+
+  const viewMatrixUniform = ctx.getUniformLocation(program, "view_matrix");
+  ctx.uniformMatrix4fv(viewMatrixUniform, false, viewMatrix);
 
   return {
     render() {
--- eye/shaders/eye.vert
+++ eye/shaders/eye.vert
@@ -2,8 +2,9 @@
 
 in vec4 position_attr;
 
+uniform mat4 view_matrix;
 uniform mat4 projection_matrix;
 
 void main() {
-    gl_Position = position_attr * projection_matrix;
+    gl_Position = projection_matrix * view_matrix * position_attr;
 }
